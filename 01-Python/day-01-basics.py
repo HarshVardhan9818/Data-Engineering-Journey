20c76ff4f681a6c7052bb886317db30ad2f17786
HarshVardhan9818 <hvardhan928@gmail.com>
2026-09-18 00:40:28 +0530

    Add Pyhton day 1 basics

---
+++ 01-Python/day-01-basics.py
@@ -0,0 +1,31 @@
+# Day 1 - Python Basics
+
+# Variables
+name = "Harsh"
+age = 28
+city = "Delhi"
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I live in {city}.")
+
+name = input("What is your name ?")
+age = int(input("What is your age? "))
+city = input("Where do you live? ")
+
+print(f"My name is {name}.")
+print(f"I am {age} years old.")
+print(f"I live in {city}.")
+
+num1 = int(input("enter first number: "))
+num2 = int(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print(F"Addition: {addition}")
+print(F"Subtraction: {subtraction}")
+print(F"Multiplication: {multiplication}")
+print(F"Division: {division}") 
